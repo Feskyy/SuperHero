aee84a7cbc79d96159f987a267bc233747463479
Feskyy <130281878+Feskyy@users.noreply.github.com>
2023-05-02 15:04:01 +0200

    r

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -6,10 +6,6 @@ const Social = () => {
   
     <div className="home__social">
            
-           <a href="https://github.com/Feskyy" className="home__social-icon">
-            <i class="fa-brands fa-github fa-sm"></i>
-            </a>
-
            <a href="https://discord.com/users/1074625652453539860" className="home__social-icon">
             <i class="fa-brands fa-discord fa-xs"></i>
             </a>
@@ -18,7 +14,9 @@ const Social = () => {
             <i class="fa-brands fa-twitter"></i>
             </a>
 
-
+            <a href="https://github.com/Feskyy" className="home__social-icon">
+            <i class="fa-brands fa-github fa-sm"></i>
+            </a>
 
             
 
