a5b26b934ae42c25b7eecbad2e70dfd321e04917
Feskyy <130281878+Feskyy@users.noreply.github.com>
2023-05-02 14:20:12 +0200

    Update Social.jsx

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -5,15 +5,15 @@ const Social = () => {
   return (
   
     <div className="home__social">
-            <a href="https://discord.com/users/1074625652453539860" className="home__social-icon" target="_blank">
-            <i class="fa-brands fa-discord fa-xs"></i>
+            <a href="https://discord.com/users/1074625652453539860" className="home__social-icon">
+            <i class="fa-brands fa-discord  fa-xs"></i>
             </a>
 
             <a href="https://twitter.com/Feskyy_" className="home__social-icon" target="_blank">
             <i class="fa-brands fa-twitter"></i>
             </a>
 
-            <a href="https://github.com/Feskyy" className="home__social-icon" target='_blank'>
+            <a href="https://github.com/Feskyy" className="home__social-icon">
             <i class="fa-brands fa-github fa-sm"></i>
             </a>
 
