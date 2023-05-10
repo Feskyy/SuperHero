7445c50b86a9102a6ec0c90d0f2e6dba79e91f45
Feskyy <130281878+Feskyy@users.noreply.github.com>
2023-05-10 20:55:42 +0200

    r

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
 
-import Navbar from './components/navbar';
-import Hom from './pages/home/Home';
-import About from './pages/about/About';
-import Contact from './pages/contact/Contact';
+import './App.css';
 
 function App() {
   return (
-    
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route index element={<Hom />} />
-        <Route path='about' element={<About />} />
-        <Route path='contact' element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img  className="App-logo" alt="logo" />
+        <p>
+         Czesc
+        </p>
+
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
