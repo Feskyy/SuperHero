1e58a0a62942f033731ece8e8e6ad9e2b5b457d2
Feskyy <130281878+Feskyy@users.noreply.github.com>
2023-05-10 21:02:18 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css'
 
-import './App.css';
+import Navbar from './components/navbar';
+import Hom from './pages/home/Home';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img  className="App-logo" alt="logo" />
-        <p>
-         Czesc
-        </p>
-
-      </header>
-    </div>
-  );
+    
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Hom />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
